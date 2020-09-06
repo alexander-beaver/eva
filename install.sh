2a70ba3859f4c5e96a7a8de3eb94e5e78841345c
Alexander Beaver <alex@beaver.dev>
2020-09-06 11:03:21 -0700

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.0/deepspeech-0.8.0-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.0/deepspeech-0.8.0-models.scorer
+sudo apt-get install libasound2-dev
+npm install
